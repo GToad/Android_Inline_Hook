c56856946579c36a5113f045c292373e4a7842f0
GToad <34957641+GToad@users.noreply.github.com>
2018-09-21 14:56:19 +0800

    fix some bugs

--- obj/local/armeabi-v7a/objs/InlineHook/InlineHook.o.d
+++ obj/local/armeabi-v7a/objs/InlineHook/InlineHook.o.d
@@ -1,8 +1,8 @@
 ./obj/local/armeabi-v7a/objs/InlineHook/InlineHook.o: \
   jni/Interface/InlineHook.cpp \
-  D:/Programs/AndroidStudio/SDK/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\vector \
+  C:/Users/GToad/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\vector \
   jni/Interface/../InlineHook\Ihook.h
 
-D:/Programs/AndroidStudio/SDK/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\vector:
+C:/Users/GToad/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\vector:
 
 jni/Interface/../InlineHook\Ihook.h:
